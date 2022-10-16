8b911bbed372cc171d53fad68204ec278f7a3210
Natalia <4yzheuskaya@gmail.com>
2022-10-16 22:46:44 +0300

    hw7 is done

--- src/homework/HW7.java
+++ src/homework/HW7.java
@@ -28,6 +28,16 @@ public class HW7 {
         System.out.println("\t ---> \t" + value);
     }
 
+    public static int[] printNegOdds(int start, int end, int step) {
+        int j = 0;
+        int l = (int) Math.round((end - start) / (double) step);
+        int[] arrNegOdds = new int[l];
+        for (int i = (start + 1 - Math.abs(start % 2)); i < end; i += step, j++) {
+            arrNegOdds[j] = i;
+        }
+        System.out.println(Arrays.toString(arrNegOdds));
+        return arrNegOdds;
+    }
 
     public static void printSequence(double start, double end, double step) {
         if (step != 0) {
@@ -54,18 +64,18 @@ public class HW7 {
         System.out.println();
     }
 
-    public static String randomArray21(int l) {
+    public static String printRandPosEven21(int l) {
         if (l > 0) {
             int[] randomArray = new int[l];
             for (int i = 0; i < l; i++) {
                 randomArray[i] = (int) (Math.random() * 7);
-                if (i == (l - 1)) {
-                    System.out.print(randomArray[i]);
-                } else {
-                    System.out.print(randomArray[i] + ", ");
-                }
+//                if (i == (l - 1)) {
+//                    System.out.print(randomArray[i]);
+//                } else {
+//                    System.out.print(randomArray[i] + ", ");
+//                }
             }
-            System.out.println();
+            System.out.println(Arrays.toString(randomArray));
         } else if (l == 0) {
             System.out.println("Длина последовательности равна нулю");
         } else {
@@ -73,6 +83,26 @@ public class HW7 {
         }
         return "";
     }
+
+    public static void printRandomIntNumber(int l, int minValue, int maxValue) {
+        if (l > 0) {
+            int[] randomArray = new int[l];
+            for (int i = 0; i < l; i++) {
+                randomArray[i] = (int) (Math.random() * (maxValue - minValue + 1) + minValue);
+//                if (i == (l - 1)) {
+//                    System.out.print(randomArray[i]);
+//                } else {
+//                    System.out.print(randomArray[i] + ", ");
+//                }
+            }
+            System.out.println(Arrays.toString(randomArray));
+        } else if (l == 0) {
+            System.out.println("Длина последовательности равна нулю");
+        } else {
+            System.out.println("Введите положительную длину последовательности");
+        }
+    }
+
 
     public static double average22(int[] array) {
         int sum = 0;
@@ -84,102 +114,145 @@ public class HW7 {
     }
 
     public static String verifyArray(int[] array, int number) {
-
+        int counter = 0;
         for (int i = 0; i < array.length; i++) {
             if (array[i] == number) {
                 System.out.print("yes ");
+                counter++; // не используется, т.к. есть break
                 break;
             }
             if (Math.abs(array[i] - number) == 1) {
                 System.out.print("maybe ");
+                counter++;
             }
 
-            if (array[i] != number && Math.abs(array[i] - number) != 1) {
-                //   System.out.print("no ");
-                continue;
+            if (i == array.length - 1 && counter == 0) {
+                System.out.print("no ");
             }
         }
         return "";
     }
 
+    public static int returnDayOfTheWeek(String day) {
+
+        switch (day.toLowerCase()) {
+            case "mon":
+                return 1;
+            case "tue":
+                return 2;
+            case "wed":
+                return 3;
+            case "thu":
+                return 4;
+            case "fri":
+                return 5;
+            case "sat":
+                return 6;
+            case "sun":
+                return 7;
+        }
+
+        return 0;
+    }
+
+    public static String returnNameOfDayOfTheWeek(int dayName) {
+
+        switch (dayName) {
+            case 1:
+                return "Mon";
+            case 2:
+                return "Tue";
+            case 3:
+                return "Wed";
+            case 4:
+                return "Thu";
+            case 5:
+                return "Fri";
+            case 6:
+                return "Sat";
+            case 7:
+                return "Sun";
+        }
+                return "Error";
+    }
+
+    public static int returnMonth(String month) {
+
+        switch (month.toLowerCase()) {
+            case "jan":
+                return 1;
+            case "feb":
+                return 2;
+            case "mar":
+                return 3;
+            case "apr":
+                return 4;
+            case "may":
+                return 5;
+            case "jun":
+                return 6;
+            case "jul":
+                return 7;
+            case "aug":
+                return 8;
+            case "sep":
+                return 9;
+            case "oct":
+                return 10;
+            case "nov":
+                return 11;
+            case "dec":
+                return 12;
+        }
+
+        return 0;
+    }
+
+    public static String returnNameOfMonth(int month) {
+
+        switch (month) {
+            case 1:
+                return "Jan";
+            case 2:
+                return "Feb";
+            case 3:
+                return "Mar";
+            case 4:
+                return "Apr";
+            case 5:
+                return "May";
+            case 6:
+                return "Jun";
+            case 7:
+                return "Jul";
+            case 8:
+                return "Aug";
+            case 9:
+                return "Sep";
+            case 10:
+                return "Oct";
+            case 11:
+                return "Nov";
+            case 12:
+                return "Dec";
+        }
+
+        return "Error";
+    }
 
     public static void main(String[] args) {
 
         /* Задача 1. Переписать  методы returnMonth(), returnDayOfTheWeek() с помощью оператора switch.
          */
         printTask();
-        String monthName;
-        int month = 8;
-        switch (month) {
-            case 1:
-                monthName = "Jan";
-                break;
-            case 2:
-                monthName = "Feb";
-                break;
-            case 3:
-                monthName = "Mar";
-                break;
-            case 4:
-                monthName = "Apr";
-                break;
-            case 5:
-                monthName = "May";
-                break;
-            case 6:
-                monthName = "Jun";
-                break;
-            case 7:
-                monthName = "Jul";
-                break;
-            case 8:
-                monthName = "Aug";
-                break;
-            case 9:
-                monthName = "Sep";
-                break;
-            case 10:
-                monthName = "Oct";
-                break;
-            case 11:
-                monthName = "Nov";
-                break;
-            case 12:
-                monthName = "Dec";
-                break;
-            default:
-                monthName = "Error";
-                break;
-        }
-
-        String dayName;
-        int day = 6;
-        switch (day) {
-            case 1:
-                dayName = "Mon";
-                break;
-            case 2:
-                dayName = "Tue";
-                break;
-            case 3:
-                dayName = "Wed";
-                break;
-            case 4:
-                dayName = "Thu";
-                break;
-            case 5:
-                dayName = "Fri";
-                break;
-            case 6:
-                dayName = "Sat";
-                break;
-            case 7:
-                dayName = "Sun";
-                break;
-            default:
-                dayName = "Error";
-                break;
-        }
+        String nameOfMonth = "Sep";
+        System.out.println(returnMonth(nameOfMonth));
+        int month = 9;
+        System.out.println(returnNameOfMonth(month));
+        String nameOfDay = "Mon";
+        System.out.println(returnDayOfTheWeek(nameOfDay));
+        int day = 1;
+        System.out.println(returnNameOfDayOfTheWeek(day));
 
         /* Задача 2. Написать универсальный метод для построения последовательности, которая начинается с числа start,
          * заканчивается числом end (оба включительно). Все числа в этой последовательности должны быть кратны числу
@@ -418,10 +491,15 @@ public class HW7 {
          * не больше 6. Заполняем значения и печатаем с помощью цикла for. Длина массива - l.
          */
         printTask();
-        randomArray21(3);
-        randomArray21(0);
-        randomArray21(30);
-        randomArray21(-1);
+        printSubTask(1);
+        printRandPosEven21(3);
+        printRandPosEven21(0);
+        printRandPosEven21(30);
+        printRandPosEven21(-1);
+        // универсальный метод
+        printSubTask(2);
+        printRandomIntNumber(20, 0, 10);
+
 
         /* Задача 22. Написать метод, который принимает на вход массив int, и печатает  среднее значение всех
          * элементов массива. Проверить работу метода, если параметр на вход - массив catsAges
@@ -438,6 +516,7 @@ public class HW7 {
         /* Задача 23. Создать массив всех нечетных отрицательных чисел в промежутке от (-1000 до -900).
          */
         printTask();
+        printSubTask(1);
         int y = 0;
         int[] array = new int[(-900 + 1000) / 2];
         for (int i = -999; i < -900; i++) {
@@ -447,21 +526,25 @@ public class HW7 {
             }
         }
         System.out.println(Arrays.toString(array));
-        System.out.println(array.length);
-
+        printSubTask(2); // решение задачи через универсальный метод
+        int start1 = -1000;
+        int end1 = -900;
+        int step1 = 2;
+        printNegOdds(start1, end1, step1);
 
         /* Задача 24. Создать массив из 10 случайных положительных целых чисел в промежутке от 100 до 200 включительно.
          */
         printTask();
+        printSubTask(1);
         int[] randomArray = new int[10];
-
         for (int i = 0; i <= 9; i++) {
             int x = (int) (Math.random() * 101 + 100);
             randomArray[i] = x;
             System.out.print(randomArray[i] + ", ");
-
         }
         System.out.println();
+        printSubTask(2); // решение задачи через универсальный метод
+        printRandomIntNumber(10, 100, 200);
 
         /* Задача 25. Написать метод, который принимает на вход массив int[] array и число int number.
          * Метод проверяет, содержится ли число number в массиве array. При первом нахождении числа number,
@@ -478,17 +561,119 @@ public class HW7 {
 
         /* Задача 26. Создать массив четных чисел и массив нечетных чисел из элементов массива из задания 24.
          */
-        //numbers array from task 24 int evenCount = 0; for (int i = 0; i < numbers.length; i++) { if (numbers[i] % 2 == 0) { evenCount += 1; } } int[] evenNumbers = new int[evenCount]; int[] oddNumbers = new int[numbers.length - evenCount]; int evenIndex = 0; int oddIndex = 0; for (int i = 0; i < numbers.length; i++) { if (numbers[i] % 2 == 0) { evenNumbers[evenIndex] = numbers[i]; evenIndex += 1; } else { oddNumbers[oddIndex] = numbers[i]; oddIndex += 1; } } System.out.println(Arrays.toString(numbers)); System.out.println(Arrays.toString(evenNumbers)); System.out.println(Arrays.toString(oddNumbers));
+        printTask();
+        int evenCount = 0;
+        for (int i = 0; i < randomArray.length; i++) {
+            if (randomArray[i] % 2 == 0) {
+                evenCount += 1;
+            }
+        }
+        int[] evenNumbers = new int[evenCount];
+        int[] oddNumbers = new int[randomArray.length - evenCount];
+        int evenIndex = 0;
+        int oddIndex = 0;
+        for (int i = 0; i < randomArray.length; i++) {
+            if (randomArray[i] % 2 == 0) {
+                evenNumbers[evenIndex] = randomArray[i];
+                evenIndex += 1;
+            } else {
+                oddNumbers[oddIndex] = randomArray[i];
+                oddIndex += 1;
+            }
+        }
+        System.out.println(Arrays.toString(randomArray));
+        System.out.println(Arrays.toString(evenNumbers));
+        System.out.println(Arrays.toString(oddNumbers));
 
         /* Задача 27. Создать двумерный массив, который состоит из имен, возрастов, цветов котов:
          * Распечатать все данные котов в коробках с четными индексами, используя двумерный массив.
          */
+        printTask();
+        printSubTask(1);
+        //перевела массив int в String
+        String[] catsAgesToString = new String[catsAges.length];
+        int catsAgesToStringIndex = 0;
+        for (int i = 0; i < catsAges.length; i++) {
+            catsAgesToString[catsAgesToStringIndex] = Integer.toString(catsAges[i]);
+            catsAgesToStringIndex++;
+        }
+        //двумерный массив String
+        String[][] саtArrayTwoD = new String[3][8];
+        for (int i = 0; i < саtArrayTwoD.length; i++) {
+            for (int j = 0; j < саtArrayTwoD[i].length; j++) {
+                if (i == 0) {
+                    саtArrayTwoD[i][j] = catsNames[j];
+                }
+                if (i == 1) {
+                    саtArrayTwoD[i][j] = catsAgesToString[j];
+                }
+                if (i == 2) {
+                    саtArrayTwoD[i][j] = catsColors[j];
+                }
+            }
+            System.out.println(Arrays.toString(саtArrayTwoD[i]));
+        }
+        for (int i = 0; i < саtArrayTwoD.length; i += 2) {
+            System.out.println();
+            for (int j = 0; j < саtArrayTwoD[i].length; j += 2) {
+                System.out.print(саtArrayTwoD[i][j] + ", ");
+            }
+        }
+        System.out.println();
+
+        printSubTask(2);
+        //через тернарные операторы
+        String[][] саtArrayTwoDTern = new String[3][8];
+        for (int i = 0; i < саtArrayTwoDTern.length; i++) {
+            for (int j = 0; j < саtArrayTwoDTern[i].length; j++) {
+                саtArrayTwoDTern[i][j] = i == 0 ? catsNames[j]
+                        : i == 1 ? String.valueOf(catsAges[j])
+                        : catsColors[j];
+                if (i % 2 == 0 && j % 2 == 0) {
+                    System.out.print(саtArrayTwoDTern[i][j] + ", ");
+                }
+            }
+        }
+        System.out.println();
 
         /* Задача 28. Создать двумерный массив целых случайных чисел от 1 до 10 включительно, размерность массива 4*8.
          */
+        printTask();
+//        int[][] randomTwoDArray = new int[4][8];
+//        start1 = 1;
+//        end1 = 10;
+//        for(int i = 0; i < randomTwoDArray.length; i++){
+//            for(int j = 0; j < randomTwoDArray[i].length; j++){
+//                randomTwoDArray[i][j] = (int)(Math.random() * (end1 - start1 + 1) + start1);
+//            }
+//            System.out.println(Arrays.toString(randomTwoDArray[i]));
+//        }
+        int[][] randomTwoDArray = new int[4][8];
+        start1 = 1;
+        end1 = 10;
+        for (int i = 0; i < randomTwoDArray.length; i++) {
+            for (int j = 0; j < randomTwoDArray[i].length; j++) {
+                randomTwoDArray[i][j] = (int) (Math.random() * (end1 - start1 + 1) + start1);
+                System.out.print(randomTwoDArray[i][j] + "\t");
+            }
+            System.out.println();
+        }
+
 
         /* Задача 29. Вывести сумму всех четных чисел массива из задания 28.
          */
+        printTask();
+        for (int i = 0; i < randomTwoDArray.length; i++) {
+            for (int j = 0; j < randomTwoDArray[i].length; j++) {
+                if (randomTwoDArray[i][j] % 2 == 0) {
+                    System.out.print(randomTwoDArray[i][j] + "\t");
+                } else {
+                    System.out.print("\t");
+                }
+            }
+            System.out.println();
+        }
+
 
         /* Задача 30. Прочитать ссылку про оператор switch в документации, посмотреть видео,
          * и переписать  проверки дат из кода, который выставляла Гаяна в субботу, 8 октября (ДЗ 6, последняя задача),
@@ -496,8 +681,89 @@ public class HW7 {
          * только надо “допилить” код под наши нужды.
          * Написать эту проверку в отдельном методе и подключить проверку в метод printEightDaysFromDate.
          */
-
+        printTask();
+        printEightDaysFromDate("Mon",2, 27, 2020, 10);
 
     }
+
+    public static int returnDate(int date) {
+        if (date < 10) {
+            String number = String.format("%03d", date);
+
+            return Integer.valueOf(number); //добавляем ноль для 1-9
+        } else {
+            return date;
+        }
+    }
+
+    public static int daysInMonth(int month, int year) {
+
+        int numDays;
+        switch (month) {
+            case 1: case 3: case 5: case 7: case 8: case 10: case 12:
+                numDays = 31;
+                break;
+            case 4: case 6: case 9: case 11:
+                numDays = 30;
+                break;
+            case 2:
+                if (((year % 4 == 0) &&
+                        !(year % 100 == 0))
+                        || (year % 400 == 0))
+                    numDays = 29;
+                else
+                    numDays = 28;
+                break;
+            default:
+
+                return -1;
+        }
+
+        return numDays;
+    }
+
+    public static void printEightDaysFromDate(String day, int month, int date, int year, int l) {
+
+        if (returnDayOfTheWeek(day) == 0 ||
+                returnNameOfMonth(month).equals("Error") ||
+                date <= 0 ||
+                date > (daysInMonth(month, year))) {
+
+            System.out.println("Please enter correct data.");
+
+        } else {
+            int currentDay = returnDayOfTheWeek(day);
+            int currentMonth = month;
+            int currentDate = date;
+            int currentYear = year; //использовал для метода без агумета year. Для текущего года.
+
+// print day
+            for (int i = 0; i < l; i++) {
+                if (currentDay != 7) {
+                    System.out.print(returnNameOfDayOfTheWeek(currentDay) + ", ");
+                    currentDay++;
+                } else {
+                    System.out.print(returnNameOfDayOfTheWeek(currentDay) + ", ");
+                    currentDay = 1;
+                }
+// print month + date
+                if (currentDate <= daysInMonth(currentMonth, currentYear)) {
+                    System.out.println(returnNameOfMonth(currentMonth) + " " + returnDate(currentDate));
+                    currentDate++;
+                } else {
+                    if (currentMonth != 12) {
+                        currentMonth++;
+                    } else {
+                        currentMonth = 1;
+                        currentYear = year + 1;
+                    }
+                    currentDate = 1;
+                    System.out.println(returnNameOfMonth(currentMonth) + " " + returnDate(currentDate));
+                    currentDate++;
+                }
+            }
+        }
+    }
+
 
 }
